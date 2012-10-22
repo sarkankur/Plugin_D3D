331d91b8debf954f550a5a7db721c58437401feb
Hendrik Polczynski <hendrikpolczyn@gmail.com>
2012-10-22 16:10:02 +0200

    comments

--- src/pp_tools.h
+++ src/pp_tools.h
@@ -1,5 +1,5 @@
 #pragma once
 
 // preprocessor tools
-#define PPCAT_NX(A, B) A ## B
-#define PPCAT(A, B) PPCAT_NX(A, B)
+#define PPCAT_NX(A, B) A ## B //!< concatenate identifier
+#define PPCAT(A, B) PPCAT_NX(A, B) //!< concatenate identifier
